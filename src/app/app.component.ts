ea675bf74c8f6ca3c1e3cdbd2dd8b596df256554
krithikakl <jenifer.a@optisolbusiness.com>
2018-12-18 20:11:42 +0530

    component demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,60 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  pageTitle = " Angular component template" ;//interpolation{{}}
-  imgUrl = 'https://picsum.photos/225' ;//property binding[]
-  count = 0;//Event binding()
-  name : string;//two-way-binding[()]
-  userName : string ;
-  private _customerName : string ;
-  @ViewChild('nameRef') nameElementRef: ElementRef; //ngafterview
-
-  ngAfterViewInit() {
-    this.nameElementRef.nativeElement.focus();
-    console.log(this.nameElementRef);
-  }
-
-
-
-  get customerName(): string{
-    return this._customerName;
-  }
-
-  set customerName(value: string){
-    this._customerName = value ;
-    if(value === "Hakkim ") {
-      alert('Hi Hakkim ');
-    }
-  }
-
-  incrementCount() {
-    this.count  +=  1;
-  }
-  greetHakkim(updatedValue){
-    this.userName = updatedValue;
-    if(updatedValue === "Hakkim ") {
-      alert('Welcome Back Hakkim ');
-    }
-  }
-
+export class AppComponent {
+  title = 'app';
 }
+
+
+
+
+
+// import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements AfterViewInit {
+//   pageTitle = " Angular component template" ;//interpolation{{}}
+//   imgUrl = 'https://picsum.photos/225' ;//property binding[]
+//   count = 0;//Event binding()
+//   name : string;//two-way-binding[()]
+//   userName : string ;
+//   private _customerName : string ;
+//   @ViewChild('nameRef') nameElementRef: ElementRef; //ngafterview
+
+//   ngAfterViewInit() {
+//     this.nameElementRef.nativeElement.focus();
+//     console.log(this.nameElementRef);
+//   }
+
+
+
+//   get customerName(): string{
+//     return this._customerName;
+//   }
+
+//   set customerName(value: string){
+//     this._customerName = value ;
+//     if(value === "Hakkim ") {
+//       alert('Hi Hakkim ');
+//     }
+//   }
+
+//   incrementCount() {
+//     this.count  +=  1;
+//   }
+//   greetHakkim(updatedValue){
+//     this.userName = updatedValue;
+//     if(updatedValue === "Hakkim ") {
+//       alert('Welcome Back Hakkim ');
+//     }
+//   }
+
+// }
